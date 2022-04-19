d635b2308b5e835abc25aba7bcc2ab4339372253
Churchill ATCHEDJI <churchill.atchedji@limpidius.com>
2022-04-19 15:05:32 +0200

    style(todoService): reformat code with eslint

--- src/todolist/todo-list.service.ts
+++ src/todolist/todo-list.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { TaskStatus, Todo, TodoTask } from './entities/todo.entity';
 import { TodoListRepository } from './todo-list-repository.service';
 import { CreateTodoDto, UpdateTodoDto } from './dto/todoDto';
@@ -21,9 +17,7 @@ export class TodoListService {
     if (todo) {
       return todo;
     }
-    throw new NotFoundException(
-      `Todo with todoId #${todoId} not found in the database!`,
-    );
+    throw new NotFoundException(`Todo with todoId #${todoId} not found in the database!`);
   }
 
   public async create(createTodoDto: CreateTodoDto): Promise<Todo> {
@@ -40,30 +34,21 @@ export class TodoListService {
     return this.todoRepository.create(todo);
   }
 
-  public async updateTodo(
-    todoId: string,
-    updateTodoDto: UpdateTodoDto,
-  ): Promise<Todo> {
+  public async updateTodo(todoId: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
     const existingTodo = await this.todoRepository.findOne(todoId);
     if (!existingTodo) {
-      throw new NotFoundException(
-        `Todo with todoId #${todoId} not found in the database!`,
-      );
+      throw new NotFoundException(`Todo with todoId #${todoId} not found in the database!`);
     }
 
     for (const updateTask of updateTodoDto.tasks) {
       if (updateTask.taskIndex >= existingTodo.tasks.length) {
-        throw new BadRequestException(
-          `The given task index doesn't exist in the todo tasks list!`,
-        );
+        throw new BadRequestException(`The given task index doesn't exist in the todo tasks list!`);
       }
 
       const todoTask = existingTodo.tasks[updateTask.taskIndex];
 
       if (updateTask.isInTodoState !== undefined) {
-        todoTask.status = updateTask.isInTodoState
-          ? TaskStatus.TODO
-          : TaskStatus.DONE;
+        todoTask.status = updateTask.isInTodoState ? TaskStatus.TODO : TaskStatus.DONE;
       }
 
       if (updateTask.label) {
